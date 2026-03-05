c8be874fdc21d8ccb35f8fc8ad85817c833c04ff
tcl-iffalcon <saittcl@gmail.com>
2026-03-05 20:11:09 +0300

    Add files via upload

--- api/channels.js
+++ api/channels.js
@@ -1,265 +1,55 @@
-// Kanal veritabanı - Güncel stream URL'leri (2025)
-// NOT: Bazı kanallar Referer kontrolü yapar, o yüzden her stream'e
-// behaviorHints ile header bilgisi eklendi.
+// Kanal veritabanı
+// Stream URL'leri iptv-org'dan dinamik olarak çekilir
 
-const UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
-
-function makeStream(url, title, referer) {
-  return {
-    url,
-    title,
-    behaviorHints: {
-      notWebReady: false,
-      proxyHeaders: {
-        request: {
-          "User-Agent": UA,
-          ...(referer ? { "Referer": referer } : {})
-        }
-      }
-    }
-  };
-}
-
-const CHANNELS = {
+const STATIC_CHANNELS = {
   "turk-kanallar": [
-    {
-      id: "livetv_trt1",
-      name: "TRT 1",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/TRT_1_logo_%282021-%29.svg/200px-TRT_1_logo_%282021-%29.svg.png",
-      genre: "General", country: "TR",
-      streams: [
-        makeStream("https://tv-trt1-dai.medya.trt.com.tr/master.m3u8", "TRT 1 HD", "https://www.trt1.com.tr/"),
-        makeStream("https://trt.daioncdn.net/trt-1/master.m3u8?app=web", "TRT 1 HD (Yedek)", "https://www.trt1.com.tr/")
-      ]
-    },
-    {
-      id: "livetv_trt2",
-      name: "TRT 2",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/TRT_2_logo.svg/200px-TRT_2_logo.svg.png",
-      genre: "General", country: "TR",
-      streams: [
-        makeStream("https://tv-trt2-dai.medya.trt.com.tr/master.m3u8", "TRT 2 HD", "https://www.trt.com.tr/"),
-        makeStream("https://trt.daioncdn.net/trt-2/master.m3u8?app=web", "TRT 2 HD (Yedek)", "https://www.trt.com.tr/")
-      ]
-    },
-    {
-      id: "livetv_show",
-      name: "Show TV",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Show_TV_logo.svg/200px-Show_TV_logo.svg.png",
-      genre: "General", country: "TR",
-      streams: [
-        makeStream("https://showtv.blutv.com/blutv_showtv_live/live.m3u8", "Show TV HD", "https://www.showtv.com.tr/"),
-        makeStream("https://ciner-live.daioncdn.net/showtv/showtv.m3u8", "Show TV HD (Yedek)", "https://www.showtv.com.tr/")
-      ]
-    },
-    {
-      id: "livetv_star",
-      name: "Star TV",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Star_TV_logo.svg/200px-Star_TV_logo.svg.png",
-      genre: "General", country: "TR",
-      streams: [
-        makeStream("https://startv.blutv.com/blutv_startv_live/live.m3u8", "Star TV HD", "https://www.startv.com.tr/")
-      ]
-    },
-    {
-      id: "livetv_kanal_d",
-      name: "Kanal D",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Kanal_D_logo.svg/200px-Kanal_D_logo.svg.png",
-      genre: "General", country: "TR",
-      streams: [
-        makeStream("https://kanald.blutv.com/blutv_kanald_live/live.m3u8", "Kanal D HD", "https://www.kanald.com.tr/")
-      ]
-    },
-    {
-      id: "livetv_fox_tr",
-      name: "FOX TV",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Fox_TV_logo_turkey.svg/200px-Fox_TV_logo_turkey.svg.png",
-      genre: "General", country: "TR",
-      streams: [
-        makeStream("https://foxtv.blutv.com/blutv_foxtv_live/live.m3u8", "FOX TV HD", "https://www.fox.com.tr/")
-      ]
-    },
-    {
-      id: "livetv_tv8",
-      name: "TV8",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/TV8_logo.svg/200px-TV8_logo.svg.png",
-      genre: "General", country: "TR",
-      streams: [
-        makeStream("https://tv8.blutv.com/blutv_tv8_live/live.m3u8", "TV8 HD", "https://www.tv8.com.tr/")
-      ]
-    },
-    {
-      id: "livetv_atv",
-      name: "ATV",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/ATV_Turkey_logo.svg/200px-ATV_Turkey_logo.svg.png",
-      genre: "General", country: "TR",
-      streams: [
-        makeStream("https://atv.blutv.com/blutv_atv_live/live.m3u8", "ATV HD", "https://www.atv.com.tr/")
-      ]
-    },
-    {
-      id: "livetv_trt_cocuk",
-      name: "TRT Çocuk",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/2/23/TRT_%C3%A7ocuk_logo.png",
-      genre: "Kids", country: "TR",
-      streams: [
-        makeStream("https://tv-trtcocuk-dai.medya.trt.com.tr/master.m3u8", "TRT Çocuk HD", "https://www.trtcocuk.net.tr/"),
-        makeStream("https://trt.daioncdn.net/trt-cocuk/master.m3u8?app=web", "TRT Çocuk (Yedek)", "https://www.trtcocuk.net.tr/")
-      ]
-    },
-    {
-      id: "livetv_trt_muzik",
-      name: "TRT Müzik",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/TRT_M%C3%BCzik_logo.svg/200px-TRT_M%C3%BCzik_logo.svg.png",
-      genre: "Music", country: "TR",
-      streams: [
-        makeStream("https://tv-trtmuzik-dai.medya.trt.com.tr/master.m3u8", "TRT Müzik HD", "https://www.trt.com.tr/"),
-        makeStream("https://trt.daioncdn.net/trt-muzik/master.m3u8?app=web", "TRT Müzik (Yedek)", "https://www.trt.com.tr/")
-      ]
-    }
+    { id: "livetv_trt1",     name: "TRT 1",        logo: "https://i.imgur.com/MVXMIq0.png", genre: "General", country: "TR", tvgId: "TRT1.tr" },
+    { id: "livetv_trt2",     name: "TRT 2",        logo: "https://i.imgur.com/1OHPQFM.png", genre: "General", country: "TR", tvgId: "TRT2.tr" },
+    { id: "livetv_show",     name: "Show TV",      logo: "https://i.imgur.com/FIeJFpg.png", genre: "General", country: "TR", tvgId: "ShowTV.tr" },
+    { id: "livetv_atv",      name: "ATV",          logo: "https://i.imgur.com/qXGu6Yw.png", genre: "General", country: "TR", tvgId: "ATV.tr" },
+    { id: "livetv_kanald",   name: "Kanal D",      logo: "https://i.imgur.com/nRm5BuX.png", genre: "General", country: "TR", tvgId: "KanalD.tr" },
+    { id: "livetv_startv",   name: "Star TV",      logo: "https://i.imgur.com/L8JT0rK.png", genre: "General", country: "TR", tvgId: "StarTV.tr" },
+    { id: "livetv_foxtv",    name: "FOX TV",       logo: "https://i.imgur.com/HMVkHca.png", genre: "General", country: "TR", tvgId: "FoxTV.tr" },
+    { id: "livetv_tv8",      name: "TV8",          logo: "https://i.imgur.com/YRbcFB9.png", genre: "General", country: "TR", tvgId: "TV8.tr" },
+    { id: "livetv_trt_cocuk",name: "TRT Çocuk",   logo: "https://i.imgur.com/tMHG2S4.png", genre: "Kids",    country: "TR", tvgId: "TRTCocuk.tr" },
+    { id: "livetv_trt_muzik",name: "TRT Müzik",   logo: "https://i.imgur.com/9lzCqKD.png", genre: "Music",   country: "TR", tvgId: "TRTMuzik.tr" },
   ],
-
   "spor-kanallar": [
-    {
-      id: "livetv_trt_spor",
-      name: "TRT Spor",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/TRT_Spor_logo.svg/200px-TRT_Spor_logo.svg.png",
-      genre: "Sports", country: "TR",
-      streams: [
-        makeStream("https://tv-trtspor1-dai.medya.trt.com.tr/master.m3u8", "TRT Spor HD", "https://www.trtspor.com.tr/"),
-        makeStream("https://trt.daioncdn.net/trt-spor/master.m3u8?app=web", "TRT Spor (Yedek)", "https://www.trtspor.com.tr/")
-      ]
-    },
-    {
-      id: "livetv_trt_spor2",
-      name: "TRT Spor Yıldız",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/TRT_Spor_Y%C4%B1ld%C4%B1z_logo.svg/200px-TRT_Spor_Y%C4%B1ld%C4%B1z_logo.svg.png",
-      genre: "Sports", country: "TR",
-      streams: [
-        makeStream("https://tv-trtspor2-dai.medya.trt.com.tr/master.m3u8", "TRT Spor Yıldız HD", "https://www.trtspor.com.tr/"),
-        makeStream("https://trt.daioncdn.net/trt-spor-yildiz/master.m3u8?app=web", "TRT Spor Yıldız (Yedek)", "https://www.trtspor.com.tr/")
-      ]
-    },
-    {
-      id: "livetv_a_spor",
-      name: "A Spor",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/A_Spor_logo.png/200px-A_Spor_logo.png",
-      genre: "Sports", country: "TR",
-      streams: [
-        makeStream("https://aspor.blutv.com/blutv_aspor_live/live.m3u8", "A Spor HD", "https://www.aspor.com.tr/")
-      ]
-    }
+    { id: "livetv_trt_spor",  name: "TRT Spor",        logo: "https://i.imgur.com/8dJhJPh.png", genre: "Sports", country: "TR", tvgId: "TRTSpor.tr" },
+    { id: "livetv_trt_spor2", name: "TRT Spor Yıldız", logo: "https://i.imgur.com/6kFjGnE.png", genre: "Sports", country: "TR", tvgId: "TRTSporYildiz.tr" },
+    { id: "livetv_aspor",     name: "A Spor",           logo: "https://i.imgur.com/bwmEh3j.png", genre: "Sports", country: "TR", tvgId: "ASpor.tr" },
   ],
-
   "haber-kanallar": [
-    {
-      id: "livetv_trt_haber",
-      name: "TRT Haber",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/TRT_Haber_logo.svg/200px-TRT_Haber_logo.svg.png",
-      genre: "News", country: "TR",
-      streams: [
-        makeStream("https://tv-trthaber-dai.medya.trt.com.tr/master.m3u8", "TRT Haber HD", "https://www.trthaber.com/"),
-        makeStream("https://trt.daioncdn.net/trt-haber/master.m3u8?app=web", "TRT Haber (Yedek)", "https://www.trthaber.com/")
-      ]
-    },
-    {
-      id: "livetv_cnn_turk",
-      name: "CNN Türk",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/CNN_T%C3%BCrk_logo.svg/200px-CNN_T%C3%BCrk_logo.svg.png",
-      genre: "News", country: "TR",
-      streams: [
-        makeStream("https://live.duhnet.tv/S2/HLS_LIVE/cnnturknp/track_1_320/playlist.m3u8", "CNN Türk HD", "https://www.cnnturk.com/"),
-        makeStream("https://cnnturk.daioncdn.net/cnnturk/cnnturk.m3u8", "CNN Türk (Yedek)", "https://www.cnnturk.com/")
-      ]
-    },
-    {
-      id: "livetv_ntv",
-      name: "NTV",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/NTV_Turkey_logo.svg/200px-NTV_Turkey_logo.svg.png",
-      genre: "News", country: "TR",
-      streams: [
-        makeStream("https://dogusyayingrubu-live.daioncdn.net/ntv/ntv.m3u8", "NTV HD", "https://www.ntv.com.tr/")
-      ]
-    },
-    {
-      id: "livetv_bloomberg",
-      name: "Bloomberg HT",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Bloomberg_HT.png/200px-Bloomberg_HT.png",
-      genre: "News", country: "TR",
-      streams: [
-        makeStream("https://bloomberght.daioncdn.net/bloomberght/bloomberght.m3u8", "Bloomberg HT HD", "https://www.bloomberght.com/")
-      ]
-    },
-    {
-      id: "livetv_aljazeera",
-      name: "Al Jazeera English",
-      logo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Al_Jazeera_English.svg/200px-Al_Jazeera_English.svg.png",
-      genre: "News", country: "QA",
-      streams: [
-        makeStream("https://live-hls-web-aje.getaj.net/AJE/index.m3u8", "Al Jazeera EN HD", "https://www.aljazeera.com/")
-      ]
-    },
-    {
-      id: "livetv_france24",
-      name: "France 24 English",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/France_24_logo.svg/200px-France_24_logo.svg.png",
-      genre: "News", country: "FR",
-      streams: [
-        makeStream("https://stream.france24.com/hls/live/2037218/F24_EN_LO_HLS/master.m3u8", "France 24 EN HD", "https://www.france24.com/")
-      ]
-    },
-    {
-      id: "livetv_dw",
-      name: "DW News English",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Deutsche_Welle_symbol_2012.svg/200px-Deutsche_Welle_symbol_2012.svg.png",
-      genre: "News", country: "DE",
-      streams: [
-        makeStream("https://dwamdstream102.akamaized.net/hls/live/2015525/dwstream102/index.m3u8", "DW News HD", "https://www.dw.com/")
-      ]
-    }
+    { id: "livetv_trt_haber",  name: "TRT Haber",        logo: "https://i.imgur.com/4Gw9l5n.png", genre: "News", country: "TR", tvgId: "TRTHaber.tr" },
+    { id: "livetv_cnn_turk",   name: "CNN Türk",         logo: "https://i.imgur.com/OZmMHJN.png", genre: "News", country: "TR", tvgId: "CNNTurk.tr" },
+    { id: "livetv_ntv",        name: "NTV",              logo: "https://i.imgur.com/R4ZxSHQ.png", genre: "News", country: "TR", tvgId: "NTV.tr" },
+    { id: "livetv_bloomberg",  name: "Bloomberg HT",     logo: "https://i.imgur.com/1pVBGdP.png", genre: "News", country: "TR", tvgId: "BloombergHT.tr" },
+    { id: "livetv_aljazeera",  name: "Al Jazeera EN",    logo: "https://i.imgur.com/2mZmFWB.png", genre: "News", country: "QA", tvgId: "AlJazeeraEnglish.qa" },
+    { id: "livetv_france24",   name: "France 24 EN",     logo: "https://i.imgur.com/bMdEbMp.png", genre: "News", country: "FR", tvgId: "France24English.fr" },
+    { id: "livetv_dw",         name: "DW News English",  logo: "https://i.imgur.com/VJH9LKf.png", genre: "News", country: "DE", tvgId: "DWEnglish.de" },
   ],
-
   "uluslararasi-kanallar": [
-    {
-      id: "livetv_trt_world",
-      name: "TRT World",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/TRT_World_Logo.svg/200px-TRT_World_Logo.svg.png",
-      genre: "News", country: "TR",
-      streams: [
-        makeStream("https://tv-trtworld-dai.medya.trt.com.tr/master.m3u8", "TRT World HD", "https://www.trtworld.com/"),
-        makeStream("https://trt.daioncdn.net/trt-world/master.m3u8?app=web", "TRT World (Yedek)", "https://www.trtworld.com/")
-      ]
-    },
-    {
-      id: "livetv_euronews",
-      name: "Euronews English",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Euronews_logo.svg/200px-Euronews_logo.svg.png",
-      genre: "News", country: "FR",
-      streams: [
-        makeStream("https://euronews-euronews-en-live.hls.cedexis.com/hls/live/euronews_en.m3u8", "Euronews EN HD", "https://www.euronews.com/")
-      ]
-    },
-    {
-      id: "livetv_nasa",
-      name: "NASA TV",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/200px-NASA_logo.svg.png",
-      genre: "Science", country: "US",
-      streams: [
-        makeStream("https://ntv1.akamaized.net/hls/live/2014075/NASA-NTV1-HLS/index.m3u8", "NASA TV HD", "https://www.nasa.gov/")
-      ]
-    },
-    {
-      id: "livetv_cgtn",
-      name: "CGTN English",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/CGTN_English_Logo.svg/200px-CGTN_English_Logo.svg.png",
-      genre: "News", country: "CN",
-      streams: [
-        makeStream("https://news.cgtn.com/resource/live/english/cgtn-news.m3u8", "CGTN EN HD", "https://www.cgtn.com/")
-      ]
-    }
+    { id: "livetv_trt_world", name: "TRT World",        logo: "https://i.imgur.com/oMiGhcN.png", genre: "News",    country: "TR", tvgId: "TRTWorld.tr" },
+    { id: "livetv_euronews",  name: "Euronews EN",      logo: "https://i.imgur.com/E5lN5j1.png", genre: "News",    country: "FR", tvgId: "EuronewsEnglish.fr" },
+    { id: "livetv_nasa",      name: "NASA TV",          logo: "https://i.imgur.com/5b3cFq2.png", genre: "Science", country: "US", tvgId: "NASATV.us" },
+    { id: "livetv_cgtn",      name: "CGTN English",     logo: "https://i.imgur.com/H6WcKp3.png", genre: "News",    country: "CN", tvgId: "CGTNEnglish.cn" },
   ]
 };
 
-const ALL_CHANNELS = Object.values(CHANNELS).flat();
+// iptv-org API'sinden stream URL'si çek
+async function fetchStreamUrl(tvgId) {
+  try {
+    const res = await fetch(`https://iptv-org.github.io/api/streams.json`);
+    if (!res.ok) return null;
+    const streams = await res.json();
+    const match = streams.find(s => s.channel === tvgId && s.status === "online");
+    return match ? match.url : null;
+  } catch {
+    return null;
+  }
+}
 
-module.exports = { CHANNELS, ALL_CHANNELS };
+// Tüm kanallar düz liste
+const ALL_CHANNELS = Object.values(STATIC_CHANNELS).flat();
+
+module.exports = { CHANNELS: STATIC_CHANNELS, ALL_CHANNELS, fetchStreamUrl };
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require("http");
-const { CHANNELS, ALL_CHANNELS } = require("./api/channels");
+const { CHANNELS, ALL_CHANNELS, fetchStreamUrl } = require("./api/channels");
 const manifest = require("./api/manifest.json");
 
 const PORT = process.env.PORT || 7000;
-const HOST = "0.0.0.0"; // Render için zorunlu
+const HOST = "0.0.0.0";
 
 const CORS_HEADERS = {
   "Access-Control-Allow-Origin": "*",
@@ -11,11 +11,77 @@ const CORS_HEADERS = {
   "Content-Type": "application/json"
 };
 
+// Önbellek: stream URL'lerini 10 dakika sakla
+const streamCache = {};
+const CACHE_TTL = 10 * 60 * 1000;
+
 function findChannel(id) {
   return ALL_CHANNELS.find(ch => ch.id === id);
 }
 
-const server = http.createServer((req, res) => {
+async function getStreams(channel) {
+  const now = Date.now();
+  const cached = streamCache[channel.id];
+  if (cached && now - cached.time < CACHE_TTL) return cached.streams;
+
+  // iptv-org API'den çek
+  try {
+    const res = await fetch(
+      `https://iptv-org.github.io/api/streams.json`
+    );
+    if (res.ok) {
+      const all = await res.json();
+      const matches = all.filter(s => s.channel === channel.tvgId);
+      if (matches.length > 0) {
+        const streams = matches.map(s => ({
+          url: s.url,
+          title: `${channel.name} (iptv-org)`,
+          behaviorHints: { notWebReady: false }
+        }));
+        streamCache[channel.id] = { streams, time: now };
+        return streams;
+      }
+    }
+  } catch (e) {}
+
+  return [];
+}
+
+// iptv-org streams.json önbelleği (tüm liste için)
+let globalStreams = null;
+let globalStreamsFetchedAt = 0;
+
+async function getGlobalStreams() {
+  const now = Date.now();
+  if (globalStreams && now - globalStreamsFetchedAt < CACHE_TTL) return globalStreams;
+  try {
+    const res = await fetch("https://iptv-org.github.io/api/streams.json");
+    if (res.ok) {
+      globalStreams = await res.json();
+      globalStreamsFetchedAt = now;
+    }
+  } catch (e) {}
+  return globalStreams || [];
+}
+
+async function getStreamsForChannel(channel) {
+  const now = Date.now();
+  const cached = streamCache[channel.id];
+  if (cached && now - cached.time < CACHE_TTL) return cached.streams;
+
+  const all = await getGlobalStreams();
+  const matches = all.filter(s => s.channel === channel.tvgId);
+  const streams = matches.map(s => ({
+    url: s.url,
+    title: `${channel.name}`,
+    behaviorHints: { notWebReady: false }
+  }));
+
+  streamCache[channel.id] = { streams, time: now };
+  return streams;
+}
+
+const server = http.createServer(async (req, res) => {
   const url = req.url.split("?")[0];
 
   if (req.method === "OPTIONS") {
@@ -25,7 +91,7 @@ const server = http.createServer((req, res) => {
 
   if (url === "/" || url === "") {
     res.writeHead(200, { ...CORS_HEADERS, "Content-Type": "text/html" });
-    return res.end(`<h2>🇹🇷 Nuvio Canlı TV Addon</h2><p>Nuvio'ya eklemek için manifest URL:</p><code>https://PROJE-ADIN.onrender.com/manifest.json</code>`);
+    return res.end(`<h2>🇹🇷 Nuvio Canlı TV Addon</h2><p>Manifest URL:</p><code>https://${req.headers.host}/manifest.json</code>`);
   }
 
   if (url === "/manifest.json") {
@@ -33,6 +99,7 @@ const server = http.createServer((req, res) => {
     return res.end(JSON.stringify(manifest));
   }
 
+  // Catalog
   const catalogMatch = url.match(/^\/catalog\/tv\/([^/]+)\.json$/);
   if (catalogMatch) {
     const channels = CHANNELS[catalogMatch[1]] || [];
@@ -45,20 +112,24 @@ const server = http.createServer((req, res) => {
     return res.end(JSON.stringify({ metas }));
   }
 
+  // Meta
   const metaMatch = url.match(/^\/meta\/tv\/([^/]+)\.json$/);
   if (metaMatch) {
-    const channel = findChannel(metaMatch[1]);
-    if (!channel) { res.writeHead(404, CORS_HEADERS); return res.end(JSON.stringify({ meta: null })); }
+    const ch = findChannel(metaMatch[1]);
+    if (!ch) { res.writeHead(404, CORS_HEADERS); return res.end(JSON.stringify({ meta: null })); }
     res.writeHead(200, CORS_HEADERS);
-    return res.end(JSON.stringify({ meta: { id: channel.id, type: "tv", name: channel.name, logo: channel.logo, poster: channel.logo, genres: [channel.genre], country: channel.country } }));
+    return res.end(JSON.stringify({ meta: { id: ch.id, type: "tv", name: ch.name, logo: ch.logo, poster: ch.logo, genres: [ch.genre], country: ch.country } }));
   }
 
+  // Stream
   const streamMatch = url.match(/^\/stream\/tv\/([^/]+)\.json$/);
   if (streamMatch) {
-    const channel = findChannel(streamMatch[1]);
-    if (!channel) { res.writeHead(404, CORS_HEADERS); return res.end(JSON.stringify({ streams: [] })); }
+    const ch = findChannel(streamMatch[1]);
+    if (!ch) { res.writeHead(404, CORS_HEADERS); return res.end(JSON.stringify({ streams: [] })); }
+
+    const streams = await getStreamsForChannel(ch);
     res.writeHead(200, CORS_HEADERS);
-    return res.end(JSON.stringify({ streams: channel.streams }));
+    return res.end(JSON.stringify({ streams }));
   }
 
   res.writeHead(404, CORS_HEADERS);
